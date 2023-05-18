00f230cf7f2d92a66c72bad7e125f5f63295ef7c
LiliKaori <lilikaori10@gmail.com>
2023-05-18 19:02:51 -0300

    Funçoes para mudar moeda e mostrar valor digitado

--- script.js
+++ script.js
@@ -1,4 +1,61 @@
-function convert() {
-    var value = document.getElementsByTagName("input")[0].value;
-    console.log(value);
+"use strict";
+const currency = [
+    {
+        "name": "Real Brasileiro",
+        "symbol": "R$",
+        "icon": "./assets/BR_icon"
+    },
+    {
+        "name": "Dólar Americano",
+        "symbol": "US$",
+        "icon": "./assets/EUA_icon"
+    },
+    {
+        "name": "Libra",
+        "symbol": "£",
+        "icon": "./assets/pound_icon"
+    },
+    {
+        "name": "Euro",
+        "symbol": "€",
+        "icon": "./assets/Euro_icon"
+    },
+    {
+        "name": "Bitcoin",
+        "symbol": "₿",
+        "icon": "./assets/BTC_icon"
+    }
+];
+const dolar = 5;
+function showValue() {
+    const value = parseFloat(document.getElementsByTagName("input")[0].value);
+    const initialCurrencyValue = document.getElementById("initialCurrencyValue");
+    initialCurrencyValue.innerHTML = value.toFixed(2);
 }
+function showCurrency() {
+    const currencyTo = document.getElementsByClassName("convertTo")[0];
+    const finalCurrencyName = document.getElementById("finalCurrencyName");
+    const finalCurrencyIcon = document.getElementById("finalCurrencyIcon");
+    console.log(currencyTo.value);
+    finalCurrencyName.innerHTML = currencyTo.value;
+    if (currencyTo.value === "US$ Dólar americano") {
+        finalCurrencyIcon.src = "./assets/EUA_icon.png";
+    }
+    if (currencyTo.value === "€ Euro") {
+        finalCurrencyIcon.src = "./assets/Euro_icon.png";
+    }
+    if (currencyTo.value === "£ Libra") {
+        finalCurrencyIcon.src = "./assets/pound_icon.png";
+    }
+    if (currencyTo.value === "₿ Bitcoin") {
+        finalCurrencyIcon.src = "./assets/BTC_icon.png";
+    }
+}
+function convert() {
+    const value = parseFloat(document.getElementsByTagName("input")[0].value);
+    const convertedValue = value / dolar;
+    console.log(convertedValue.toFixed(2));
+    const finalCurrencyValue = document.getElementById("finalCurrencyValue");
+    finalCurrencyValue.innerHTML = convertedValue.toFixed(2);
+}
+//# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,4 +1,68 @@
-function convert(){
-    const value: string= document.getElementsByTagName("input")[0].value;
-    console.log(value)
+const currency = [
+    {
+        "name": "Real Brasileiro",
+        "symbol": "R$",
+        "icon": "./assets/BR_icon"
+    },
+    {
+        "name": "Dólar Americano",
+        "symbol": "US$",
+        "icon": "./assets/EUA_icon"
+    },
+    {
+        "name": "Libra",
+        "symbol": "£",
+        "icon": "./assets/pound_icon"
+    },
+    {
+        "name": "Euro",
+        "symbol": "€",
+        "icon": "./assets/Euro_icon"
+    },
+    {
+        "name": "Bitcoin",
+        "symbol": "₿",
+        "icon": "./assets/BTC_icon"
+    }
+]
+
+const dolar = 5
+    
+    
+function showValue (){
+    const value= parseFloat(document.getElementsByTagName("input")[0].value)
+    const initialCurrencyValue = document.getElementById("initialCurrencyValue") as HTMLElement    
+    initialCurrencyValue.innerHTML= value.toFixed(2)    
 }
+
+function showCurrency (){
+    const currencyTo= document.getElementsByClassName("convertTo")[0] as HTMLSelectElement
+    const finalCurrencyName = document.getElementById("finalCurrencyName") as HTMLParagraphElement
+    const finalCurrencyIcon = document.getElementById("finalCurrencyIcon") as HTMLImageElement
+    console.log(currencyTo.value)
+
+    finalCurrencyName.innerHTML= currencyTo.value
+        
+    if(currencyTo.value==="US$ Dólar americano"){
+        finalCurrencyIcon.src="./assets/EUA_icon.png"
+    }
+    if(currencyTo.value==="€ Euro"){
+        finalCurrencyIcon.src="./assets/Euro_icon.png"
+    }
+    if(currencyTo.value==="£ Libra"){
+        finalCurrencyIcon.src="./assets/pound_icon.png"
+    }
+    if(currencyTo.value==="₿ Bitcoin"){
+        finalCurrencyIcon.src="./assets/BTC_icon.png"
+    }    
+}
+
+function convert(){
+    const value= parseFloat(document.getElementsByTagName("input")[0].value)    
+    const convertedValue= value/dolar
+    console.log(convertedValue.toFixed(2))    
+    
+    const finalCurrencyValue = document.getElementById("finalCurrencyValue") as HTMLElement
+    finalCurrencyValue.innerHTML=convertedValue.toFixed(2)
+}
+
