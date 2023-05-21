d9b97d14c902cba46239aa2b23e3b13a98df4603
LiliKaori <lilikaori10@gmail.com>
2023-05-21 12:05:49 -0300

    Finalização scripts

--- script.js
+++ script.js
@@ -1,61 +1,101 @@
 "use strict";
 const currency = [
     {
-        "name": "Real Brasileiro",
-        "symbol": "R$",
-        "icon": "./assets/BR_icon"
+        name: 'Real Brasileiro',
+        symbol: "R$",
+        icon: "./assets/BR_icon.png",
+        value: 1
     },
     {
-        "name": "Dólar Americano",
-        "symbol": "US$",
-        "icon": "./assets/EUA_icon"
+        name: "Dólar Americano",
+        symbol: "US$",
+        icon: "./assets/EUA_icon.png",
+        value: 4.9982887
     },
     {
-        "name": "Libra",
-        "symbol": "£",
-        "icon": "./assets/pound_icon"
+        name: "Libra Esterlina",
+        symbol: "£",
+        icon: "./assets/GBP_icon.png",
+        value: 6.2209076
     },
     {
-        "name": "Euro",
-        "symbol": "€",
-        "icon": "./assets/Euro_icon"
+        name: "Euro",
+        symbol: "€",
+        icon: "./assets/Euro_icon.png",
+        value: 5.4084907
     },
     {
-        "name": "Bitcoin",
-        "symbol": "₿",
-        "icon": "./assets/BTC_icon"
+        name: "Bitcoin",
+        symbol: "₿",
+        icon: "./assets/BTC_icon.png",
+        value: 134697.74
     }
 ];
-const dolar = 5;
+const currencyTo = document.getElementsByClassName("convertTo")[0];
 function showValue() {
     const value = parseFloat(document.getElementsByTagName("input")[0].value);
     const initialCurrencyValue = document.getElementById("initialCurrencyValue");
-    initialCurrencyValue.innerHTML = value.toFixed(2);
+    if (value) {
+        initialCurrencyValue.innerHTML = value.toFixed(2);
+        convert();
+    }
+    else {
+        initialCurrencyValue.innerHTML = "0.00";
+    }
 }
 function showCurrency() {
-    const currencyTo = document.getElementsByClassName("convertTo")[0];
     const finalCurrencyName = document.getElementById("finalCurrencyName");
     const finalCurrencyIcon = document.getElementById("finalCurrencyIcon");
-    console.log(currencyTo.value);
-    finalCurrencyName.innerHTML = currencyTo.value;
+    showValue();
     if (currencyTo.value === "US$ Dólar americano") {
-        finalCurrencyIcon.src = "./assets/EUA_icon.png";
+        const index = currency.findIndex((currency) => currency.name === 'Dólar Americano');
+        finalCurrencyName.innerHTML = currency[index].name;
+        finalCurrencyIcon.src = currency[index].icon;
     }
     if (currencyTo.value === "€ Euro") {
-        finalCurrencyIcon.src = "./assets/Euro_icon.png";
+        const index = currency.findIndex((currency) => currency.name === 'Euro');
+        finalCurrencyName.innerHTML = currency[index].name;
+        finalCurrencyIcon.src = currency[index].icon;
     }
     if (currencyTo.value === "£ Libra") {
-        finalCurrencyIcon.src = "./assets/pound_icon.png";
+        const index = currency.findIndex((currency) => currency.name === 'Libra Esterlina');
+        finalCurrencyName.innerHTML = currency[index].name;
+        finalCurrencyIcon.src = currency[index].icon;
     }
     if (currencyTo.value === "₿ Bitcoin") {
-        finalCurrencyIcon.src = "./assets/BTC_icon.png";
+        const index = currency.findIndex((currency) => currency.name === 'Bitcoin');
+        finalCurrencyName.innerHTML = currency[index].name;
+        finalCurrencyIcon.src = currency[index].icon;
     }
+    convert();
 }
 function convert() {
     const value = parseFloat(document.getElementsByTagName("input")[0].value);
-    const convertedValue = value / dolar;
-    console.log(convertedValue.toFixed(2));
     const finalCurrencyValue = document.getElementById("finalCurrencyValue");
-    finalCurrencyValue.innerHTML = convertedValue.toFixed(2);
+    if (value) {
+        if (currencyTo.value === "US$ Dólar americano") {
+            const index = currency.findIndex((currency => currency.name === 'Dólar Americano'));
+            const convertedValue = value / currency[index].value;
+            finalCurrencyValue.innerHTML = convertedValue.toFixed(2);
+        }
+        if (currencyTo.value === "€ Euro") {
+            const index = currency.findIndex((currency => currency.name === 'Euro'));
+            const convertedValue = value / currency[index].value;
+            finalCurrencyValue.innerHTML = convertedValue.toFixed(2);
+        }
+        if (currencyTo.value === "£ Libra") {
+            const index = currency.findIndex((currency => currency.name === 'Libra Esterlina'));
+            const convertedValue = value / currency[index].value;
+            finalCurrencyValue.innerHTML = convertedValue.toFixed(2);
+        }
+        if (currencyTo.value === "₿ Bitcoin") {
+            const index = currency.findIndex((currency => currency.name === 'Bitcoin'));
+            const convertedValue = value / currency[index].value;
+            finalCurrencyValue.innerHTML = convertedValue.toFixed(2);
+        }
+    }
+    else {
+        finalCurrencyValue.innerHTML = "0.00";
+    }
 }
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,68 +1,118 @@
-const currency = [
+interface Icurrency {    
+    name: string;
+    symbol: string;
+    icon: string;
+    value:number
+    
+}
+
+const currency: Icurrency[]= [
     {
-        "name": "Real Brasileiro",
-        "symbol": "R$",
-        "icon": "./assets/BR_icon"
+        name: 'Real Brasileiro',
+        symbol: "R$",
+        icon: "./assets/BR_icon.png",
+        value: 1
     },
     {
-        "name": "Dólar Americano",
-        "symbol": "US$",
-        "icon": "./assets/EUA_icon"
+        name: "Dólar Americano",
+        symbol: "US$",
+        icon: "./assets/EUA_icon.png",
+        value: 4.9982887
     },
     {
-        "name": "Libra",
-        "symbol": "£",
-        "icon": "./assets/pound_icon"
+        name: "Libra Esterlina",
+        symbol: "£",
+        icon: "./assets/GBP_icon.png",
+        value: 6.2209076
     },
     {
-        "name": "Euro",
-        "symbol": "€",
-        "icon": "./assets/Euro_icon"
+        name: "Euro",
+        symbol: "€",
+        icon: "./assets/Euro_icon.png",
+        value: 5.4084907
     },
     {
-        "name": "Bitcoin",
-        "symbol": "₿",
-        "icon": "./assets/BTC_icon"
+        name: "Bitcoin",
+        symbol: "₿",
+        icon: "./assets/BTC_icon.png",
+        value: 134697.74
     }
 ]
+    
+const currencyTo= document.getElementsByClassName("convertTo")[0] as HTMLSelectElement
 
-const dolar = 5
-    
-    
+
 function showValue (){
     const value= parseFloat(document.getElementsByTagName("input")[0].value)
     const initialCurrencyValue = document.getElementById("initialCurrencyValue") as HTMLElement    
-    initialCurrencyValue.innerHTML= value.toFixed(2)    
+    if(value){
+        initialCurrencyValue.innerHTML= value.toFixed(2)
+        convert()
+    }else{
+        initialCurrencyValue.innerHTML= "0.00"
+    }    
+        
 }
 
-function showCurrency (){
-    const currencyTo= document.getElementsByClassName("convertTo")[0] as HTMLSelectElement
+function showCurrency (){    
     const finalCurrencyName = document.getElementById("finalCurrencyName") as HTMLParagraphElement
-    const finalCurrencyIcon = document.getElementById("finalCurrencyIcon") as HTMLImageElement
-    console.log(currencyTo.value)
-
-    finalCurrencyName.innerHTML= currencyTo.value
+    const finalCurrencyIcon = document.getElementById("finalCurrencyIcon") as HTMLImageElement    
+    showValue()
+    
         
     if(currencyTo.value==="US$ Dólar americano"){
-        finalCurrencyIcon.src="./assets/EUA_icon.png"
+        const index = currency.findIndex((currency)=>currency.name==='Dólar Americano')        
+        finalCurrencyName.innerHTML= currency[index].name
+        finalCurrencyIcon.src=currency[index].icon        
     }
     if(currencyTo.value==="€ Euro"){
-        finalCurrencyIcon.src="./assets/Euro_icon.png"
+        const index = currency.findIndex((currency)=>currency.name==='Euro') 
+        finalCurrencyName.innerHTML= currency[index].name       
+        finalCurrencyIcon.src=currency[index].icon 
     }
     if(currencyTo.value==="£ Libra"){
-        finalCurrencyIcon.src="./assets/pound_icon.png"
+        const index = currency.findIndex((currency)=>currency.name==='Libra Esterlina')   
+        finalCurrencyName.innerHTML= currency[index].name     
+        finalCurrencyIcon.src=currency[index].icon 
     }
     if(currencyTo.value==="₿ Bitcoin"){
-        finalCurrencyIcon.src="./assets/BTC_icon.png"
+        const index = currency.findIndex((currency)=>currency.name==='Bitcoin')
+        finalCurrencyName.innerHTML= currency[index].name        
+        finalCurrencyIcon.src=currency[index].icon 
     }    
+    convert()
 }
 
-function convert(){
-    const value= parseFloat(document.getElementsByTagName("input")[0].value)    
-    const convertedValue= value/dolar
-    console.log(convertedValue.toFixed(2))    
-    
+function convert(){  
+    const value= parseFloat(document.getElementsByTagName("input")[0].value)  
     const finalCurrencyValue = document.getElementById("finalCurrencyValue") as HTMLElement
-    finalCurrencyValue.innerHTML=convertedValue.toFixed(2)
+    
+    if(value){
+        if(currencyTo.value==="US$ Dólar americano"){
+            const index = currency.findIndex((currency=>currency.name==='Dólar Americano'))
+            const convertedValue= value/currency[index].value    
+            finalCurrencyValue.innerHTML=convertedValue.toFixed(2)
+        }
+    
+        if(currencyTo.value==="€ Euro"){
+            const index = currency.findIndex((currency=>currency.name==='Euro'))
+            const convertedValue= value/currency[index].value    
+            finalCurrencyValue.innerHTML=convertedValue.toFixed(2)
+        }
+    
+        if(currencyTo.value==="£ Libra"){
+            const index = currency.findIndex((currency=>currency.name==='Libra Esterlina'))
+            const convertedValue= value/currency[index].value    
+            finalCurrencyValue.innerHTML=convertedValue.toFixed(2)
+        }
+    
+        if(currencyTo.value==="₿ Bitcoin"){
+            const index = currency.findIndex((currency=>currency.name==='Bitcoin'))
+            const convertedValue= value/currency[index].value    
+            finalCurrencyValue.innerHTML=convertedValue.toFixed(2)
+        }
+    }else{
+        finalCurrencyValue.innerHTML= "0.00"
+    }
 }
 
